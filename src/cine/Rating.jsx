c91e85ceb657ed8c6a2d60785319460570eba1a6
Jahid Hasan <jh3495689@gmail.com>
2024-02-23 12:37:46 +0600

    movie card section add

---
+++ src/cine/Rating.jsx
@@ -0,0 +1,16 @@
+import star from '../assets/star.svg';
+
+const Rating = ({value}) => {
+    const stars = Array(value).fill(star);
+    return (
+        <>
+         {
+            stars.map(star, index) => 
+                <img key={index} src={star} width='14' height='14' />
+            
+         }   
+        </>
+    );
+};
+
+export default Rating;
