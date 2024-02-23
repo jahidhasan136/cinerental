edac9d83c7cd03f2565a9b76c2e16630603c9abe
Jahid Hasan <jh3495689@gmail.com>
2024-02-23 13:03:43 +0600

    add movie modal

--- src/cine/Rating.jsx
+++ src/cine/Rating.jsx
@@ -1,16 +1,14 @@
-import star from '../assets/star.svg';
+import star from "../assets/star.svg";
 
-const Rating = ({value}) => {
-    const stars = Array(value).fill(star);
-    return (
-        <>
-         {
-            stars.map(star, index) => 
-                <img key={index} src={star} width='14' height='14' />
-            
-         }   
-        </>
-    );
+const Rating = ({ value }) => {
+  const stars = Array(value).fill(star);
+  return (
+    <>
+      {stars.map((star, index) => (
+        <img key={index} src={star} width="14" height="14" />
+      ))}
+    </>
+  );
 };
 
 export default Rating;
